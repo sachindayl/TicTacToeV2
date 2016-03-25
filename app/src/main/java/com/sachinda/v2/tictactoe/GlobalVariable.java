6da0dce3a731a7755667c3a92e986fdb3b7579fb
Sachinda Liyanaarachchi <sachinda.yl@gmail.com>
2016-03-24 22:33:51 -0400

    keeping repo up to date - sachindayl

--- app/src/main/java/com/sachinda/v2/tictactoe/GlobalVariable.java
+++ app/src/main/java/com/sachinda/v2/tictactoe/GlobalVariable.java
@@ -1,11 +1,31 @@
 package com.sachinda.v2.tictactoe;
 
 import android.app.Application;
+import android.content.Context;
+import android.os.Build;
+import android.widget.Button;
 
 import java.util.Random;
 
 public class GlobalVariable extends Application {
     private int style = 1;
+
+    private Context mContext;
+    private R.drawable icon;
+
+    public GlobalVariable (R.drawable icon){
+        this.icon = icon;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = getApplicationContext();
+    }
+
+    public Context getContext() {
+        return mContext;
+    }
 
     public int randomNumber() {
 
@@ -21,5 +41,16 @@ public class GlobalVariable extends Application {
 
     public void setStyle(int style) {
         this.style = style;
+    }
+
+    public static void changeImage(Button x, Button y, Button z, int icon){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            this.icon =  mContext.getResources().getDrawable(icon, mContext.getTheme());
+        } else {
+            return resources.getDrawable(id);
+        }
+        this.icon = mContext.getResources().getDrawable(icon);
+        x.setBackgroundResource(R.drawable.icon);
     }
 }
